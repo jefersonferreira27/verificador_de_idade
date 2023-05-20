738e0015d6e1d2c0012432119d78cf428ba010fb
Jeferson Ferreira <jefersonferreira27@outlook.com>
2023-05-20 19:39:31 -0300

    Projeto de verificador de idade

---
+++ age-calculator-app-main/script/jquery.js
@@ -0,0 +1,35 @@
+$(function(){
+    //Mood dark
+    $("#dark").on("click", function(){
+        let background = $("body")
+       if(background.css != "black"){
+        background.css("background-color", "black")
+       }
+    });
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Validação do form
+    function formA (){
+        let day = $("#day")
+        let month = $("#month")
+        let year = $("#year")
+
+    }
+})
